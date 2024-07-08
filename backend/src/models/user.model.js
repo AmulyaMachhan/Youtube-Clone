dbf9842641fc80f36038e26ad806043720e8a583
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-08 15:11:04 +0530

    Added Hook for hashing password with bcrypt library

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -52,4 +52,11 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
 export const User = mongoose.model("User", userSchema);
