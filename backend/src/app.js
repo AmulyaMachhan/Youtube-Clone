c3b712f27f1a3e4092e7072510e4d30fd0461f80
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-08 14:35:09 +0530

    Setup application for express server

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
