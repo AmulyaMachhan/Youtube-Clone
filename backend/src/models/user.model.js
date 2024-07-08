d3e5fa57b237a16dcb728d5b00fae5eee4574e34
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-08 15:01:49 +0530

    Created User Model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary image
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary image
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
