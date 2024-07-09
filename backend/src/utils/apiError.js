e42eab67a1055088e2af338b26899214315827f7
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-09 13:54:20 +0530

    Created a utility class for handling errors throughout the youtube app

---
+++ backend/src/utils/apiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    stack = "",
+    errors = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.data = null;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
