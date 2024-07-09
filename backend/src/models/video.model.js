488f900d28a9c8ae7f638edd4727f310f05c16fb
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-09 13:15:01 +0530

    Added Mongoose aggregate paginate plugin for video model

--- backend/src/models/video.model.js
+++ backend/src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -40,5 +41,7 @@ const videoSchema = new Schema(
     timestamps: true,
   }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
