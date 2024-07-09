ae37af9aa2107b14bbd11319adc0549d78d585e0
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-09 12:50:14 +0530

    Added methods for generating acccess and refresh Token

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -78,4 +78,16 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
