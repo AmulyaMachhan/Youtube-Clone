72de6d1e5eae26c33ce528857f9faea5972a0b4c
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-09 15:13:18 +0530

    Created async handler for handling asynchronous fucnctions throughout the code

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,11 +1,21 @@
-// ASYNCHRONOUS FUNCTION HANDLER USING TRY CATCH BLOCK
-const asyncHandler = (func) => async (req, res, next) => {
-  try {
-    await func(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: true,
-      message: error.message,
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
     });
-  }
+  };
 };
+
+export { asyncHandler };
+
+// ASYNCHRONOUS FUNCTION HANDLER USING TRY CATCH BLOCK
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: true,
+//       message: error.message,
+//     });
+//   }
+// };
