7c49504c6ba1a23b97767941ab2c20e34e3665ed
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-09 12:00:14 +0530

    Added Compare method to user model for password validation

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -63,4 +63,19 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
