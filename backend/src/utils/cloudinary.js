613e7ae729682a7662a725343f208058d1f3dd74
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-11 19:14:11 +0530

    added cloudinary configurations for upload files in cloudinary

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -6,3 +6,27 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //File has been successufully uploaded on cloudinary
+    console.log(`File is uploaded on cloudinary ${response.url}`);
+
+    //File has been removed from the local storage
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath);
+
+    console.log(`Cloudinary upload error ${err}`);
+
+    return null;
+  }
+};
