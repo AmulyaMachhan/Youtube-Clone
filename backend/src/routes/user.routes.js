531c21ba51430eebbca25ab4b2d05bf739766e99
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-11 22:22:22 +0530

    added register router

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { registerUser } from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
