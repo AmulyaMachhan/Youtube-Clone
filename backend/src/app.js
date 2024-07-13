884fbd8fc8fd53eec7ed02ea596727a5d3fcd1be
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-13 12:16:32 +0530

    Import and created routes in express application

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,4 +10,10 @@ app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
